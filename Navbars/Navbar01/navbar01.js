e1e410cf7012577f89c20b2eb7de5d456ad32a23
Tidbits JS <tidbitsjs@gmail.com>
2021-05-04 19:03:47 +0530

    resposive navbar

---
+++ Navbars/Navbar01/navbar01.js
@@ -0,0 +1,22 @@
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+      toggle.classList.toggle("bx-x");
+    });
+  }
+};
+
+showMenu("header-toggle", "nav-menu");
+
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
